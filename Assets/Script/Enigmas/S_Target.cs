0a5db559679206394f97b0aba2f102c2dcef7a4b
Bastien Bodéré <bastien.bodere@gmail.com>
2024-03-25 16:41:57 +0100

    25/03 aprem
    update la rotation des cibles avec une coroutine

--- Assets/Script/Enigmas/S_Target.cs
+++ Assets/Script/Enigmas/S_Target.cs
@@ -1,33 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class S_Target : MonoBehaviour
 {
 
 
 
-
     private bool touched = false;
     private bool turned = false;
+    private int incr = 0;
+    public float speedRota = 1f;
+
+
+
+
+    //private Quaternion test = transform.rotation;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (touched && !turned)
         {
             turned = true;
 
-            Debug.Log("tourne");
-            this.transform.Rotate(180, 0, 0);
+
+
+            StartCoroutine(startRota());
+
+
+
         }
+
+
+
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -38,4 +56,19 @@ public class S_Target : MonoBehaviour
             touched = true;
         }
     }
+
+
+    private IEnumerator startRota()
+    {
+        while (incr <= 180)
+        {
+            yield return new WaitForEndOfFrame();
+            incr++;
+            this.transform.Rotate(1, 0, 0);
+
+        }
+
+
+    }
+
 }
