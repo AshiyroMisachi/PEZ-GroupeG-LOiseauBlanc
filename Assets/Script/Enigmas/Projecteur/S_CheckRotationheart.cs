5bb6508c17598a5e5546669d6ab9875e46b6e7da
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-10 13:12:53 +0200

    updateLumiereProjo

---
+++ Assets/Script/Enigmas/Projecteur/S_CheckRotationheart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CheckRotationheart : MonoBehaviour
+{
+
+
+
+    public S_PlayerManager playerManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManager = S_ManagerManager.GetManager<S_PlayerManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Interactibles/Child/Collectible/S_ICollectible_Heart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ICollectible_Heart : S_Interractible_Collectible
+{
+
+
+
+
+
+
+
+    public override void Interraction()
+    {
+
+
+    }
+
+
+
+}
