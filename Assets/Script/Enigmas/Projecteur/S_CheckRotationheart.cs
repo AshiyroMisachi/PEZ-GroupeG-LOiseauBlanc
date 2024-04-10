c4cb70b8ebec21915db759f8e85173f7f7ead527
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-10 15:17:08 +0200

    update piedestral pour enigme lumiere

--- Assets/Script/Enigmas/Projecteur/S_CheckRotationheart.cs
+++ Assets/Script/Enigmas/Projecteur/S_CheckRotationheart.cs
@@ -1,23 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class S_CheckRotationheart : MonoBehaviour
+public class S_CheckRotationheart : Interractibles
 {
 
 
+    [SerializeField]
+    private GameObject heartItem, heartObject;
+    private bool isActive;
+    [SerializeField]
+    private Camera puzzleCamera; 
+    [SerializeField]
+    private float inspectSpeed = 1, inspectZoomSpeed = 0;
 
-    public S_PlayerManager playerManager;
-    // Start is called before the first frame update
-    void Start()
+
+
+    public void Update()
     {
-        playerManager = S_ManagerManager.GetManager<S_PlayerManager>();
+
+        //to quit the puzzle
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SetupPuzzle();
+        }
+
+
+        if (!isActive)
+        {
+            return;
+        }
+
+
+
+        if (heartObject.activeInHierarchy)
+        {
+            Debug.Log("test");
+
+            //ajout rota
+
+            //code test bonne rota
+
+            float x = Input.GetAxis("Horizontal") * inspectSpeed;
+            float z = Input.GetAxis("Vertical") * inspectSpeed;
+            float zoom = Input.GetAxis("Mouse ScrollWheel") * inspectZoomSpeed;
+
+            heartObject.transform.Rotate(x, 0, z, Space.Self);
+
+
+
+        }
+
+
+
+
+
+
+
+
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void Interraction()
     {
-        
+        var playerInterract = S_ManagerManager.GetManager<S_PlayerManager>().GetPlayer().GetComponent<S_PlayerInterract>();
+
+
+        if (playerInterract.GetObjectInHand() != null && heartItem != null && playerInterract.GetObjectInHand().gameObject == heartItem)
+        {
+            heartObject.SetActive(true);
+            playerInterract.DestroyObjectInHand();
+            return;
+        }
+        SetupPuzzle();
+    }
+
+
+    private void SetupPuzzle()
+    {
+        var playerManager = S_ManagerManager.GetManager<S_PlayerManager>();
+        this.GetComponent<BoxCollider>().enabled = !this.GetComponent<BoxCollider>().enabled;
+        if (!isActive)
+        {
+            isActive = true;
+            playerManager.SetPlayerState(PlayerState.Puzzle);
+            S_CameraFunction.SwitchCamera(puzzleCamera, playerManager.GetPlayerCamera());
+            S_CameraFunction.LockCursor();
+            return;
+        }
+
+        isActive = false;
+        S_CameraFunction.SwitchCamera(playerManager.GetPlayerCamera(), puzzleCamera);
+        S_CameraFunction.LockCursor();
+        playerManager.SetPlayerState(PlayerState.Exploration);
     }
 }
--- Assets/Script/Interactibles/Child/Collectible/S_ICollectible_Heart.cs
+++ Assets/Script/Interactibles/Child/Collectible/S_ICollectible_Heart.cs
@@ -4,31 +4,55 @@ using UnityEngine;
 
 public class S_ICollectible_Heart : S_Interractible_Collectible
 {
+            
+    // z -> 90 ou -90 + x -90
+    
+    // x -90 y 90 z 180
+    //-90,180,90
+    //
+    //x tj égal -90 ou 270
+    //y-90 = z
+    //OU
+    //y = z-90
 
+    private S_PlayerManager playerManager;
+    private S_PlayerInterract playerInterract;
+    private Transform rotationObjectInHand;
 
-
-
-
-    public S_PlayerManager playerManager;
-    public S_PlayerInterract playerInterract;
+    [SerializeField]
+    private GameObject referenceRotaHeart;
     void Start()
     {
         playerManager = S_ManagerManager.GetManager<S_PlayerManager>();
         playerInterract = playerManager.GetPlayer().GetComponent<S_PlayerInterract>();
+
         
     }
 
     public override void InterractionInspection()
     {
+        rotationObjectInHand = playerInterract.GetObjectPosition();
 
-        if (true)
+        Debug.Log("x = " + rotationObjectInHand.transform.localEulerAngles.x);
+        Debug.Log("y = " + rotationObjectInHand.transform.localEulerAngles.y);
+        Debug.Log("z = " + rotationObjectInHand.transform.localEulerAngles.z);
+        var x = rotationObjectInHand.transform.localEulerAngles.x;
+        var y = rotationObjectInHand.transform.localEulerAngles.y;
+        var z = rotationObjectInHand.transform.localEulerAngles.z;
+
+        if ((x <= -85 && x>= -95) || (x >= 265 && x <= 275))
         {
-            Debug.Log("bonne position !");
-            //set pos jeur
-            //set rota keur
-            //mettre keur kinematic true
-        }
+            if ((y % 90 == 0) && (z %90 == 0)) 
+            {
+                if ((y + 90 == z ) || (y == z + 90))
+                {
+                    Debug.Log("inY");
 
+                }
+
+
+            }
+        }
 
 
     }
