dd0c6a780e86df84b9af6a93b80c17e275b16dfb
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-10 13:24:38 +0200

    updateHeart

--- Assets/Script/Interactibles/Child/Collectible/S_ICollectible_Heart.cs
+++ Assets/Script/Interactibles/Child/Collectible/S_ICollectible_Heart.cs
@@ -9,13 +9,30 @@ public class S_ICollectible_Heart : S_Interractible_Collectible
 
 
 
-
-
-    public override void Interraction()
+    public S_PlayerManager playerManager;
+    public S_PlayerInterract playerInterract;
+    void Start()
     {
+        playerManager = S_ManagerManager.GetManager<S_PlayerManager>();
+        playerInterract = playerManager.GetPlayer().GetComponent<S_PlayerInterract>();
+        
+    }
+
+    public override void InterractionInspection()
+    {
+
+        if ()
+        {
+            Debug.Log("bonne position !");
+            //set pos jeur
+            //set rota keur
+            //mettre keur kinematic true
+        }
+
 
 
     }
+
 
 
 
