0cd4933de7fb170d211fb5344a3d55eb1cecb54e
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-12 13:22:04 +0200

    added check rotation enigme lumière

--- Assets/Script/Enigmas/Projecteur/S_CheckRotationheart.cs
+++ Assets/Script/Enigmas/Projecteur/S_CheckRotationheart.cs
@@ -15,6 +15,8 @@ public class S_CheckRotationheart : Interractibles
     [SerializeField]
     private float inspectSpeed = 1, inspectZoomSpeed = 0;
 
+    private Vector3 minX = Vector3.zero;
+
 
 
     public void Update()
@@ -36,11 +38,8 @@ public class S_CheckRotationheart : Interractibles
 
         if (heartObject.activeInHierarchy)
         {
-            Debug.Log("test");
 
-            //ajout rota
 
-            //code test bonne rota
 
             float x = Input.GetAxis("Horizontal") * inspectSpeed;
             float z = Input.GetAxis("Vertical") * inspectSpeed;
@@ -48,6 +47,56 @@ public class S_CheckRotationheart : Interractibles
 
             heartObject.transform.Rotate(x, 0, z, Space.Self);
 
+
+            
+
+            if (heartObject.transform.localEulerAngles.x <= 280 && heartObject.transform.localEulerAngles.x >= 260)
+            {
+                Debug.Log("x");
+
+                if ((heartObject.transform.localEulerAngles.y >= -10 && heartObject.transform.localEulerAngles.y <= 20) && (heartObject.transform.localEulerAngles.z >= 80 && heartObject.transform.localEulerAngles.z <= 100))
+                {
+                    Debug.Log("y");
+                }
+
+
+                if ((heartObject.transform.localEulerAngles.y >= 80 && heartObject.transform.localEulerAngles.y <= 100) && (heartObject.transform.localEulerAngles.z >= 170 && heartObject.transform.localEulerAngles.z <= 190))
+                {
+                    Debug.Log("y");
+                }
+
+
+                if ((heartObject.transform.localEulerAngles.y >= 170 && heartObject.transform.localEulerAngles.y <= 190) && (heartObject.transform.localEulerAngles.z >= 250 && heartObject.transform.localEulerAngles.z <= 270))
+                {
+                    Debug.Log("y");
+                }
+
+
+                //
+
+                if ((heartObject.transform.localEulerAngles.y >= 80 && heartObject.transform.localEulerAngles.y <= 100) && (heartObject.transform.localEulerAngles.z >= -10 && heartObject.transform.localEulerAngles.z <= 20))
+                {
+                    Debug.Log("y");
+                }
+
+
+                if ((heartObject.transform.localEulerAngles.y >= 170 && heartObject.transform.localEulerAngles.y <= 190) && (heartObject.transform.localEulerAngles.z >= 80 && heartObject.transform.localEulerAngles.z <= 100))
+                {
+                    Debug.Log("y");
+                }
+
+
+                if ((heartObject.transform.localEulerAngles.y >= 250 && heartObject.transform.localEulerAngles.y <= 270) && (heartObject.transform.localEulerAngles.z >= 170 && heartObject.transform.localEulerAngles.z <= 190))
+                {
+                    Debug.Log("y");
+                }
+
+
+
+
+            }
+
+            //code test bonne rota
 
 
         }
