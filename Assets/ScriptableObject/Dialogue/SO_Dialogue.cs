4915f678c5c11e816f531e3a22c949cd89021ba2
AshiyroMisachi <riallot.alexandre@outlook.fr>
2024-04-13 13:40:27 +0200

    Update SO_Dialogue.cs

--- Assets/ScriptableObject/Dialogue/SO_Dialogue.cs
+++ Assets/ScriptableObject/Dialogue/SO_Dialogue.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New_Dialogue", menuName = "Dialogue", order = 0)]
 public class SO_Dialogue : ScriptableObject
 {
+    [TextArea]
     public string[] dialogue;
 }
