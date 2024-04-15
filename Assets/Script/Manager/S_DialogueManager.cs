6073664e7efd03334f44ef97a81d1e17eb5e84ea
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-15 15:01:53 +0200

    Revert "up train conlifct resolved"
    This reverts commit 23534c875a6f02dd5d7f1feab8ebf8af2f667588, reversing
    changes made to b1bd33bc83931f3d625d2f46f8bee5831a50b32c.

--- Assets/Script/Manager/S_DialogueManager.cs
+++ Assets/Script/Manager/S_DialogueManager.cs
@@ -20,13 +20,13 @@ public class S_DialogueManager : Manager
     private SO_Dialogue[] dialogueList;
     [SerializeField]
     private float[] timeDialogueList;
-    private int currentDialogue = 0;
+    private int currentDialogue;
 
+    [SerializeField]
     private List<SO_Dialogue> dialogueShowList;
 
     private void Start()
     {
-        dialogueShowList = new List<SO_Dialogue>();
         StartCoroutine(DialogueActivation());
     }
 
