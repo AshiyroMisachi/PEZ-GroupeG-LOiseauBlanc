6fd27f4c15487b277beb9dc72c785a12f47b8eff
Bastien Bodéré <bastien.bodere@gmail.com>
2024-04-15 16:56:53 +0200

    update Train

---
+++ Assets/Script/Enigmas/Train/S_CheckStation.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class S_CheckStation : MonoBehaviour
+{
+    public S_Train trainManager;
+    public int id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trainManager = S_ManagerManager.GetManager<S_Train>();
+        trainManager.splineTrain.Pause();
+        trainManager.splineWagon1.Pause();
+        trainManager.splineWagon2.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Train")
+        {
+            if(trainManager.currentPos<=id)
+            {
+                trainManager.splineTrain.Pause();
+                trainManager.splineWagon1.Pause();
+                trainManager.splineWagon2.Pause();
+            }
+
+
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Train")
+        {
+            if (trainManager.currentPos > id)
+            {
+                trainManager.splineTrain.Play();
+                trainManager.splineWagon1.Play();
+                trainManager.splineWagon2.Play();
+            }
+
+
+        }
+    }
+
+
+}
---
+++ Assets/Script/Enigmas/Train/S_Train.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class S_Train : Manager
+{
+    public SplineAnimate splineTrain,splineWagon1,splineWagon2;
+    public int currentPos = 0;
+    public List<GameObject> stopPoints = new List<GameObject>();
+    public List<bool> stopPointFree = new List<bool>();
+
+
+    void Start()
+    {
+
+        splineTrain.Pause();
+        splineWagon1.Pause();
+        splineWagon2.Pause();
+
+        for(int i = 0;i < stopPoints.Count; i++)
+        {
+            stopPointFree.Add(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (stopPointFree[currentPos])
+        {
+            currentPos++;
+
+        }
+
+        
+
+
+
+        
+    }
+
+
+    public void isEnigmaCompleted(int id)
+    {
+        stopPointFree[id] = true;
+        return;
+    }
+
+}
